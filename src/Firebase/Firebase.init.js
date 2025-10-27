38ec4d8ba1685493da76a1f0f68a0f2595a7bfa0
SAJJAD HOSAIN JIM <sajjadjim15@gmail.com>
2025-10-27 14:28:24 +0600

    Firebase app token secure

--- src/Firebase/Firebase.init.js
+++ src/Firebase/Firebase.init.js
@@ -1,27 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-
 // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: import.meta.env.VITE_apiKey,
-//   authDomain: import.meta.env.VITE_authDomain,
-//   projectId: import.meta.env.VITE_projectId,
-//   storageBucket: import.meta.env.VITE_storageBucket,
-//   messagingSenderId: import.meta.env.VITE_messagingSenderId,
-//   appId: import.meta.env.VITE_appId
-// };
-
-
-
 const firebaseConfig = {
-  apiKey: "AIzaSyDwr4cnsvUFAGTGegl7jiDhJoZKH9JAo-o",
-  authDomain: "food-sharing-platform-3f385.firebaseapp.com",
-  projectId: "food-sharing-platform-3f385",
-  storageBucket: "food-sharing-platform-3f385.firebasestorage.app",
-  messagingSenderId: "892686228891",
-  appId: "1:892686228891:web:e9da2448a6b336e588d53e"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
